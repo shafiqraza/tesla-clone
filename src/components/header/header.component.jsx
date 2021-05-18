5e33a776bda81cbf58e465bb2d418da44cbebfff
Shafiq Raza <shafiqraza503@gmail.com>
2021-05-18 07:58:31 +0500

    navar done for now next -> sidebar/menu

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HeaderContainer, HeaderLinks, HeaderRight, LogoContainer, LogoImg, HeaderMenu } from "./header.styles";
+
+import logo from "../../images/logo.svg";
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+const Header = ({isMenuOpen, toggleIsMenuOpen}) => (
+    <HeaderContainer>
+        <LogoContainer>
+            <Link to="/">
+                <LogoImg src={logo} alt="tesla logo"/>
+            </Link>
+        </LogoContainer>
+        <HeaderLinks>
+            <Link to="/">Model S</Link>
+            <Link to="/">Model 3</Link>
+            <Link to="/">Model X</Link>
+            <Link to="/">Model Y</Link>
+            <Link to="/">Solar Roof</Link>
+            <Link to="/">Solar Panel</Link>
+        </HeaderLinks>
+        <HeaderRight isMenuOpen={isMenuOpen}>
+            <Link to="/">Shop</Link>
+            <Link to="/">Tesla Account</Link>
+            <HeaderMenu onClick={toggleIsMenuOpen}>
+                {
+                    isMenuOpen ? <CloseIcon /> : <MenuIcon />
+                }
+            </HeaderMenu>
+        </HeaderRight>
+    </HeaderContainer>
+);
+
+export default Header;
---
+++ src/components/header/header.styles.js
@@ -0,0 +1,60 @@
+import styled, { css } from "styled-components/macro";
+
+const BasisLinkStyles = css`
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: 600;
+  color: #000;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  word-spacing: 2px;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
+  padding-top: 15px;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const LogoContainer = styled.div`
+  & a {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+export const LogoImg = styled.img`
+  width: 90px;
+  flex: 1;
+  object-fit: contain;
+  margin-right: 100px;
+`;
+
+export const HeaderLinks = styled.div`
+  display: flex;
+  gap: 15px;
+
+  & a {
+    ${BasisLinkStyles}
+  }
+`;
+
+export const HeaderRight = styled.div`
+  display: flex;
+  gap: 15px;
+
+  & a {
+    ${BasisLinkStyles}
+
+    visibility: ${(props) => (props.isMenuOpen ? "hidden" : "visible")}
+  }
+`;
+
+export const HeaderMenu = styled.div`
+  cursor: pointer;
+`;
