4331b1155db049e3410272e637d29539bb927061
Shafiq Raza <shafiqraza503@gmail.com>
2021-05-28 08:56:52 +0500

    added logout button to menu-item.cmp and conditionally render it

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MenuItemContainer } from "./menu-item.styles";
+import { MenuItemContainer, LogoutButton } from "./menu-item.styles";
 
-const MenuItem = ({ text, to = "#" }) => (
-  <MenuItemContainer>
-    <Link to={to}>{text}</Link>
-  </MenuItemContainer>
-);
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { signOut } from "../../redux/user/user-slice";
+const MenuItem = ({ text, to = "#" }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
+  const clickHandler = () => {
+    dispatch(signOut());
+  };
+
+  return (
+    <MenuItemContainer>
+      {history.location.pathname === "/teslaaccount" &&
+      text === "tesla account" ? (
+        <>
+          <Link to={to}>
+            {text} <LogoutButton onClick={clickHandler}>Log out</LogoutButton>
+          </Link>
+        </>
+      ) : (
+        <Link to={to}>{text}</Link>
+      )}
+    </MenuItemContainer>
+  );
+};
 export default MenuItem;
--- src/components/menu-item/menu-item.styles.js
+++ src/components/menu-item/menu-item.styles.js
@@ -1,17 +1,39 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const LinkStyles = css`
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 400;
+  color: #333;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  word-spacing: 2px;
+  border-bottom: 1px solid #d0d1d2;
+  width: 100%;
+  display: block;
+  padding: 16px 10px;
+`;
 
 export const MenuItemContainer = styled.div`
   & a {
-    text-decoration: none;
-    font-size: 14px;
-    font-weight: 400;
-    color: #333;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    word-spacing: 2px;
-    border-bottom: 1px solid #d0d1d2;
-    width: 100%;
-    display: block;
-    padding: 16px 10px;
+    ${LinkStyles}
+  }
+`;
+
+export const LogoutButton = styled.button`
+  display: block;
+  border: none;
+  outline: none;
+  margin-top: 25px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  color: #666;
+  background-color: transparent;
+  padding: 0;
+  text-transform: uppercase;
+
+  &:hover {
+    color: #000;
   }
 `;
