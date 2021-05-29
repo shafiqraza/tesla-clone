1968885bbf88a11912c3d62cbf4e8a6618a0ef13
Shafiq Raza <shafiqraza503@gmail.com>
2021-05-29 21:36:06 +0500

    removed redux-logger when app is in production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,14 @@ import userReducer from "./user/user-slice";
 import menuReducer from "./menu/menu-slice";
 import logger from "redux-logger";
 
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") middlewares.push(logger);
+
 export const store = configureStore({
   reducer: {
     user: userReducer,
     menu: menuReducer,
   },
-  middleware: [logger],
+  middleware: middlewares,
 });
